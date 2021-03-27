13c75daf12388eab1fd1af6b5cf53e7daf98fe56
Paulo Moreira <prmoreira98@gmail.com>
2021-03-27 17:12:59 -0300

    pessoa as abstract class

--- src/model/enfermeiro.py
+++ src/model/enfermeiro.py
@@ -1,11 +1,11 @@
 
 class Enfermeiro():
-    def __init__(self, matricula_coren: int, nome_completo: str, cpf: int, data_nascimento:str):
+
+    def __init(self, nome_completo, cpf, idade, endereco, matricula_coren, num_atendimentos):
         self.__matricula_coren = matricula_coren
-        self.__nome_completo = nome_completo
-        self.__data_nascimento = data_nascimento
-        self.__cpf = cpf
-        self.__num_atendimentos = 0 
+        self.__num_atendimentos = num_atendimentos
+        super(nome_completo, cpf, idade, endereco)
+
 
     @property
     def matricula_coren(self):
@@ -14,30 +14,6 @@ class Enfermeiro():
     @matricula_coren.setter
     def matricula_coren(self, matricula_coren):
         self.__matricula_coren = matricula_coren
-
-    @property
-    def nome_completo(self):
-        return self.__nome_completo
-
-    @nome_completo.setter
-    def nome_completo(self, nome_completo):
-        self.__nome_completo = nome_completo
-
-    @property
-    def data_nascimento(self):
-        return self.__data_nascimento
-
-    @data_nascimento.setter
-    def data_nascimento(self, data_nascimento):
-        self.__data_nascimento = data_nascimento
-
-    @property
-    def cpf(self):
-        return self.__cpf
-        
-    @cpf.setter
-    def cpf(self, cpf):
-        self.__cpf = cpf
 
     @property
     def num_atendimentos(self):
