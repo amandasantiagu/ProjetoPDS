aa504b5e1944a388695d07c9f4222e5258582cb0
amandasantiagu <amandynhah023@hotmail.com>
2021-05-04 01:12:33 -0300

    Add item 6 in controller and TelaVacina

--- src/control/main_controller.py
+++ src/control/main_controller.py
@@ -1,19 +1,23 @@
 from .control_enfermeiro import  EnfermeiroController
 from .control_paciente import PacienteController
+from .control_vacina import VacinaController
 from .control_agendamento import AgendamentoController
 from .control_posto import PostoController
 from .control_relatorio import RelatorioController
 from ..view.tela_sistema import TelaSistemaView
 
+
 class MainController:
 
     def __init__(self):
-        self.__posto = PostoController().incluir()
-        self.__view = TelaSistemaView(self.__posto)
+        self.__controller_posto = PostoController()
+        self.__view = TelaSistemaView(self.__controller_posto.posto)
         self.__controller_paciente = PacienteController()
         self.__controller_enfermeiro = EnfermeiroController()
-        self.__controller_agendamentos = AgendamentoController(self.__posto, self.__controller_paciente, self.__controller_enfermeiro)
+        self.__controller_vacina = VacinaController()
+        self.__controller_agendamentos = AgendamentoController(self.__controller_posto.posto, self.__controller_paciente, self.__controller_enfermeiro)
         self.__controller_relatorio = RelatorioController(self.__controller_paciente, self.__controller_agendamentos)
+        
 
     def run(self):
         escolha = 'x'
@@ -22,8 +26,10 @@ class MainController:
             opcoes = {
                     1: self.__controller_paciente.option,
                     2: self.__controller_enfermeiro.option,
-                    3: self.__controller_agendamentos.option,
-                    4: self.__controller_relatorio.relatorio,
+                    3: self.__controller_vacina.option,
+                    4: self.__controller_agendamentos.option,
+                    5: self.__controller_relatorio.relatorio,
+                    6: self.__controller_posto.incluir,
                     0: self.sair
                     }
             escolha = self.__view.menu_principal()
@@ -35,6 +41,6 @@ class MainController:
 
     
     def sair(self):
-        exit()
+        return
 
 
