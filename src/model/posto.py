c4b539ab2a7b4330d649aff52b4c2381e0d55131
amandasantiagu <amandynhah023@hotmail.com>
2021-05-04 01:11:51 -0300

    setter nome in model posto

--- src/model/posto.py
+++ src/model/posto.py
@@ -13,6 +13,10 @@ class Posto:
     @property
     def nome(self):
         return self.__nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
 
 
     def novas_doses(self, num):
