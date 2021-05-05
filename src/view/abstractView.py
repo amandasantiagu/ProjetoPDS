7d836fbd374307c872d8128f92f5d9b709f7dd34
Paulo Moreira <prmoreira98@gmail.com>
2021-05-05 02:08:53 -0300

    new abstract view class

--- src/view/abstractView.py
+++ src/view/abstractView.py
@@ -1,17 +1,15 @@
-import PySimpleGUI as sg
 from abc import ABC, abstractmethod
 
 class AbstractView(ABC):
 
-    def __init__(self, title_text):
-        self.__title_text = title_text
-        self.__window = None
+    #dict keys as integers mean nothing to me
+    def set_keys_to_attrs(self, values, attributes):
+        count = 0
+        for key, value in values.items():
+            values[key] = attributes[count]
+            del values[count]
+            count += 1
 
-
-    def show(self, layout):
-        self.__window = sg.Window(self.__title_text).Layout(layout)
-        
-
-    def read(self):
-        button, values = window.Read()
         return values
+
+
