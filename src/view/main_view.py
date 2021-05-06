89750061187e725ff7d980bbc4be05546e266a5f
amandasantiagu <amandynhah023@hotmail.com>
2021-05-06 02:16:06 -0300

    constructor main View and ATT controller

--- src/view/main_view.py
+++ src/view/main_view.py
@@ -1,20 +1,21 @@
-
+import PySimpleGUI as sg
 class TelaSistemaView:
 
     def __init__(self, posto):
         self.__posto = posto
 
     def menu_principal(self):
-        print("\n ---- Posto de Saúde " + self.__posto.nome + "----")
-        print ("1 - Cadastro do Paciente")
-        print ("2 - Cadastro do Enfermeiro")
-        print ("3 - Cadastro de Vacinas")
-        print ("4 - Agendamentos")
-        print ("5 - Gerar Relatório Geral")
-        print ("6 - Alterar nome do Posto")
-        print ("0 - Sair")
-        try:
-            opcao = int(input('Digite uma opção: '))
-            return opcao
-        except ValueError as e:
-            print('Dado Invalido, digite novamente.{}'.format(e))
+        layout =[
+                [sg.Text("---- Posto de Saúde " + self.__posto.nome + "----")],
+                [sg.Button('Cadastrar Paciente')],
+                [sg.Button('Cadastro do Enfermeiro')],
+                [sg.Button('Cadastro de Vacinas')],
+                [sg.Button('Agendamentos')],
+                [sg.Button('Gerar Relatório Geral')],
+                [sg.Button('Alterar nome do Posto')],
+                [sg.Button('Sair')],
+                ]
+        window = sg.Window('Tela de Sistema').Layout(layout)
+        button_str = window.read()
+        window.close()
+        return button_str[0]
