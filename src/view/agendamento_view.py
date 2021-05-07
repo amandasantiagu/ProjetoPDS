e08b9cc73dccf32a6082e8919b18a16b5d59f7ba
amandasantiagu <amandynhah023@hotmail.com>
2021-05-07 12:51:00 -0300

    PYSIMPLEGUI application in ControlAgendamento + AgendamentoView Principais

--- src/view/agendamento_view.py
+++ src/view/agendamento_view.py
@@ -1,16 +1,20 @@
+import PySimpleGUI as sg
 from .abstractView import AbstractView
 
 class AgendamentoView(AbstractView):
 
     def tela_agendamento(self):
-        print ("\n ---- Cadastro de Agendamentos ----")
-        print ("1 - Incluir Agendamento")
-        print ("2 - Excluir Agendamento")
-        print ("3 - Alterar Agendamento")
-        print ("4 - Listagem de Agendamento por Paciente")
-        print ("0 - Sair")
-        opcao = self.le_inteiro('Digite uma opção: ', [1, 2, 3, 4, 0])
-        return opcao
+        layout = [
+                    [sg.Button('Incluir Agendamento')],
+                    [sg.Button('Listar Agendamentos')],
+                    [sg.Button('Atualizar Agendamento')],
+                    [sg.Button('Remover Agendamento')],
+                    [sg.Button('Sair')],
+                ]
+        window = sg.Window('Cadastro de Agendamentos').Layout(layout)
+        button_str = window.read()
+        window.close()
+        return button_str[0]
 
     def incluir(self):
         data = input("Data Escolhida: ")
