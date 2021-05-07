a184b3769f2b31b35a8929bbde87b5ea9dda9631
amandasantiagu <amandynhah023@hotmail.com>
2021-05-07 12:51:51 -0300

    SympleGUI in Posto View and Control posto

--- src/view/posto_view.py
+++ src/view/posto_view.py
@@ -1,8 +1,19 @@
+import PySimpleGUI as sg
 class PostoView:
 
     def incluir(self):
-        print("---------------------------------")
-        print("Cadastro de Posto")
-        nome = input("Nome: ")
-        return nome
+        layout = [
+            [sg.Text('Nome:', size=(15, 1)), sg.InputText()],
+            [sg.Submit(), sg.Cancel()]
+        ]
+        window = sg.Window('Atualização de nome do Posto').Layout(layout)
+        button_str, items = window.read()
+        if button_str == 'Submit':  
+            values = self.set_keys_to_attrs(items, {0: 'nome'})
+            print(values)
+            window.close()
+            return values
+        else:
+            window.close()
+            return None
 
